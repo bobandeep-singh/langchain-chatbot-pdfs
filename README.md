ca93686859da17afd34bbc569988c769b7aff4f4
Bobandeep Singh <bobandeep_singh@apple.com>
2024-01-09 12:39:21 +0530

    Created GUI interface

---
+++ README.md
@@ -0,0 +1,8 @@
+## Dependencies
+* streamlit - for GUI
+* pypdf2 - to read pdf files
+* langchain - to interact with our language model
+* python-dotenv - to load secrets from .env
+* faiss-cpu - vector store
+* openai
+* huggingface_hub
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import streamlit as st
+
+# set page configuration
+
+
+def main(name):
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+
+    st.header("CHat with multiple PDFs :books:")
+    st.text_input("Ask a question about your documents:")
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        st.file_uploader("Upload your PDFs and click on process")
+        st.button("Process")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main('PyCharm')
